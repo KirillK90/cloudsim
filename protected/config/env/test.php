81f3be14b4082fc4396486c1f3b53967225718dc
KirillK <kurkin@polygant.ru>
2015-01-05 18:20:00 +0300

    init

--- protected/config/env/test.php
+++ protected/config/env/test.php
@@ -4,5 +4,5 @@ define('YII_DEBUG', true);
 
 define('DB_HOST',               'localhost');
 define('DB_NAME',               'kkurki17_cloudsi');
-define('DB_USERNAME',           'kkurki17');
+define('DB_USERNAME',           'kkurki17_cloudsi');
 define('DB_PASSWORD',           'OikiJaOn');
