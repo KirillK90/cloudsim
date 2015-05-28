4e2e4b06be1765f3329fe2ab73c456be217275c0
evgenybalashov <evgenybalashovv@gmail.com>
2015-05-28 16:33:47 +0400

    form tasks

--- protected/views/tasks/form2.php
+++ protected/views/tasks/form2.php
@@ -5,10 +5,11 @@
  * @var TbActiveForm $form
  */
 
-echo $form->textFieldControlGroup($model, 'name');
-echo $form->textFieldControlGroup($model->data, 'description');
-echo $form->textFieldControlGroup($model->data, 'index_nano_n');
-echo $form->textFieldControlGroup($model->data, 'index_nano_m');
-echo $form->textFieldControlGroup($model->data, 'length_link');
-echo $form->textFieldControlGroup($model->data, 'indent_radius_inside');
-echo $form->textFieldControlGroup($model->data, 'indent_radius_outwards');
+echo $form->textFieldControlGroup($model->data, 'nuclear_charge');
+echo $form->textFieldControlGroup($model->data, 'valence_electrons');
+echo $form->textFieldControlGroup($model->data, 's_orbital');
+echo $form->textFieldControlGroup($model->data, 'p_orbital');
+echo $form->textFieldControlGroup($model->data, 'd_orbital');
+echo $form->textFieldControlGroup($model->data, 'f_orbital');
+echo $form->textFieldControlGroup($model->data, 'g_orbital');
+
